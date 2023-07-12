cdb890a9cedd986f48151d88a0a8bbbaca437138
Viktor Veskovic <viktor@rebellion7.com>
2023-07-12 15:15:03 +0200

    seneca setup

---
+++ src/assets/scripts/renderers/visionaries-landing.js
@@ -0,0 +1,14 @@
+import DefaultRenderer from './default'
+
+class VisionariesLandingRenderer extends DefaultRenderer {
+    onEnter() {}
+    onLeave() {}
+    onEnterCompleted() {
+      super.onEnterCompleted()
+    }
+    onLeaveCompleted() {
+      super.onLeaveCompleted()
+    }
+}
+
+export default VisionariesLandingRenderer;
